3b934c97f35493f38642a9a862093be22dd5ece5
Gulyamov M.A <gulyamov@buroburo.ru>
2025-11-14 08:10:47 +0500

    fix: adjust height of Partners component slider for better layout

--- src/features/partners/Partners.tsx
+++ src/features/partners/Partners.tsx
@@ -13,7 +13,7 @@ export const Partners = () => {
       <div className="flex justify-center lg:mb-20  mb-5">
         <div className="border-b-4 border-brand-orange w-[83px]" />
       </div>
-      <div className="mt-5 mb-5 lg:h-64 h-32">
+      <div className="mt-5  lg:h-58 h-32">
         <SliderShared slides={partnersApi} />
       </div>
     </div>
